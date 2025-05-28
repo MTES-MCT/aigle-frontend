a6d17dd276b310d5983e2381a7afc002d6cff434
stephenprn <stephen.perrin@2017.icam.fr>
2025-05-28 15:10:32 +0200

    feat: set short name for geo custom zone and geo custom zone categories

--- src/models/geo/geo-custom-zone-category.ts
+++ src/models/geo/geo-custom-zone-category.ts
@@ -2,5 +2,6 @@ import { Timestamped, Uuided } from '@/models/data';
 
 export interface GeoCustomZoneCategory extends Uuided, Timestamped {
     name: string;
+    nameShort: string;
     color: string;
 }
